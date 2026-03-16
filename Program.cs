b225b1c2f8bf93714e25bfa20aee5062a99cad57
Adonis <adrianspl@o2.pl>
2026-03-16 20:32:19 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        var test = new Node<int>(1, new Node<int>(2, new Node<int>(3)));
+        PrintSingleLinkedList(test);
+    }
+
+    static void PrintSingleLinkedList<T>(Node<T> head)
+    {
+        Console.Write("head -> ");
+        var current = head;
+        while (current != null)
+        {
+            Console.Write(current);
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
+
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T>? Next { get; set; }
+
+    public Node(T data, Node<T>? next = null)
+    {
+        Data = data;
+        Next = next;
+    }
+
+    public override string ToString()
+    {
+        return $"{Data} -> ";
+    }
+}
