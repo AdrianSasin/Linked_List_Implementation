505a965778c02ae05e5e43ad5b3e3cfd6bee9d0d
Adonis <adrianspl@o2.pl>
2026-03-17 19:31:36 +0100

    4/8 challenges complete

--- Program.cs
+++ Program.cs
@@ -21,6 +21,52 @@ class Program
         }
         Console.WriteLine("null");
     }
+
+    static void AddAtEndOfSingleLinkedList<T>(T element, ref Node<T>? head)
+    {
+        if (head == null)
+        {
+            head = new Node<T>(element);
+            return;
+        }
+
+        var current = head;
+        while (current.Next != null)
+        {
+            current = current.Next;
+        }
+        current.Next = new Node<T>(element);
+    }
+    static Node<T> CreateSingleLinkedList<T>(params T[] arr)
+    {
+        if (arr == null ||  arr.Length == 0)
+        {
+            return null;
+        }
+        var head = new Node<T>(arr[0]);
+        var current = head;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            current.Next = new Node<T>(arr[i]);
+            current = current.Next;
+        }
+        return head;
+    }
+
+    static Node<T> ReverseSingleLinkedList<T>(Node<T> head)
+    {
+        Node<T> prev = null, next;
+        while (head != null)
+        {
+            next = head.Next;
+            head.Next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+
 }
 
 public class Node<T>
